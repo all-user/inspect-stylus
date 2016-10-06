9e847246f0ebb948c0d56a38075b5e37a1cf6f55
Keita Okamoto <ok@aaa.email.ne.jp>
2016-10-07 00:28:08 +0900

    Add tests.

--- test/use_cli/index.test.js
+++ test/use_cli/index.test.js
@@ -1,5 +1,4 @@
 import assert from 'assert';
-import stylus from 'stylus';
 import Promise from 'bluebird';
 import path from 'path';
 import _fs from 'fs';
@@ -10,7 +9,7 @@ const fs = Promise.promisifyAll(_fs);
 const PLUGIN_OUT_PATH = path.resolve(__dirname, './data.json');
 
 describe('CLI --use', () => {
-  it('arguments.length', () => {
+  it('arguments.length === 1 when no argument', () => {
     return Promise
       .try(() => {
         return new Promise(done => {
@@ -24,7 +23,117 @@ describe('CLI --use', () => {
       .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
       .then(json => {
         const data = JSON.parse(json);
-        assert.deepEqual(data.result, 1);
+        assert.strictEqual(data.argsLength, 1);
+      });
+  });
+
+  it('arguments.length === 1 when one argument', () => {
+    return Promise
+      .try(() => {
+        return new Promise(done => {
+          child_process.spawn('stylus', ['--use', './plugin.js', '--with', 'value',  './vars.styl'], {
+            cwd: __dirname
+          }).on('exit', () => {
+            done();
+          });
+        });
+      })
+      .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
+      .then(json => {
+        const data = JSON.parse(json);
+        assert.strictEqual(data.argsLength, 1);
+      });
+  });
+
+  it('number arg', () => {
+    return Promise
+      .try(() => {
+        return new Promise(done => {
+          child_process.spawn('stylus', ['--use', './plugin.js', '--with', '1234', './vars.styl'], {
+            cwd: __dirname
+          }).on('exit', () => {
+            done();
+          });
+        });
+      })
+      .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
+      .then(json => {
+        const data = JSON.parse(json);
+        assert.strictEqual(data.argType, 'number');
+        assert.strictEqual(data.arg1, 1234);
+      });
+  });
+
+  it('string arg', () => {
+    return Promise
+      .try(() => {
+        return new Promise(done => {
+          child_process.spawn('stylus', ['--use', './plugin.js', '--with', '\'value\'', './vars.styl'], {
+            cwd: __dirname
+          }).on('exit', () => {
+            done();
+          });
+        });
+      })
+      .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
+      .then(json => {
+        const data = JSON.parse(json);
+        assert.strictEqual(data.argType, 'string');
+        assert.strictEqual(data.arg1, 'value');
+      });
+  });
+
+  it('object arg', () => {
+    return Promise
+      .try(() => {
+        return new Promise(done => {
+          child_process.spawn('stylus', ['--use', './plugin.js', '--with', '{}', './vars.styl'], {
+            cwd: __dirname
+          }).on('exit', () => {
+            done();
+          });
+        });
+      })
+      .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
+      .then(json => {
+        const data = JSON.parse(json);
+        assert.strictEqual(data.argType, 'object');
+      });
+  });
+
+  it('array arg', () => {
+    return Promise
+      .try(() => {
+        return new Promise(done => {
+          child_process.spawn('stylus', ['--use', './plugin.js', '--with', '[]', './vars.styl'], {
+            cwd: __dirname
+          }).on('exit', () => {
+            done();
+          });
+        });
+      })
+      .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
+      .then(json => {
+        const data = JSON.parse(json);
+        assert(Array.isArray(data.arg1));
+      });
+  });
+
+  it('function arg', () => {
+    return Promise
+      .try(() => {
+        return new Promise(done => {
+          child_process.spawn('stylus', ['--use', './plugin.js', '--with', 'function() {}', './vars.styl'], {
+            cwd: __dirname
+          }).on('exit', () => {
+            done();
+          });
+        });
+      })
+      .then(() => fs.readFileAsync(PLUGIN_OUT_PATH, 'utf8'))
+      .then(json => {
+        const data = JSON.parse(json);
+        assert.strictEqual(data.argType, 'function');
       });
   });
 });
--- test/use_cli/plugin.js
+++ test/use_cli/plugin.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function() {
-  const o = { result: arguments.length };
+module.exports = function(arg1, arg2) {
+  const o = {
+    argsLength: arguments.length,
+    argType: typeof arg1,
+    arg1: arg1
+  };
   fs.writeFileSync(path.resolve(__dirname, './data.json'), JSON.stringify(o), 'utf8');
   return function() {};
 };
